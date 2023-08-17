d59142ad0b3c5cd3735a4da038b60997e1b627eb
Sarthak Shastri <122080718+sarvalabs-sarthak@users.noreply.github.com>
2023-08-17 12:07:15 +0530

    lsp integrated

---
+++ client/out/extension.js
@@ -0,0 +1,42 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.deactivate = exports.activate = void 0;
+const path = require("path");
+const vscode_1 = require("vscode");
+const node_1 = require("vscode-languageclient/node");
+let client;
+function activate(context) {
+    // The server is implemented in node
+    const serverModule = context.asAbsolutePath(path.join('server', 'out', 'server.js'));
+    // If the extension is launched in debug mode then the debug server options are used
+    // Otherwise the run options are used
+    const serverOptions = {
+        run: { module: serverModule, transport: node_1.TransportKind.ipc },
+        debug: {
+            module: serverModule,
+            transport: node_1.TransportKind.ipc,
+        }
+    };
+    // Options to control the language client
+    const clientOptions = {
+        // Register the server for plain text documents
+        documentSelector: [{ scheme: 'file', language: 'coco' }],
+        synchronize: {
+            // Notify the server about file changes to '.clientrc files contained in the workspace
+            fileEvents: vscode_1.workspace.createFileSystemWatcher('**/.clientrc')
+        }
+    };
+    // Create the language client and start the client.
+    client = new node_1.LanguageClient('cocolangServer', 'Cocolang Server', serverOptions, clientOptions);
+    // Start the client. This will also launch the server
+    client.start();
+}
+exports.activate = activate;
+function deactivate() {
+    if (!client) {
+        return undefined;
+    }
+    return client.stop();
+}
+exports.deactivate = deactivate;
+//# sourceMappingURL=extension.js.map
