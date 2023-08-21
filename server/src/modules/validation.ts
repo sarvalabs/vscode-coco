10b5c5417366603f4fa1c39689cc9ea1962bc7b6
Sarthak Shastri <122080718+sarvalabs-sarthak@users.noreply.github.com>
2023-08-21 21:16:15 +0530

    feature: implement language server protocol (#4)
    see #4 for more

---
+++ server/src/modules/validation.ts
@@ -0,0 +1,54 @@
+import {
+	Diagnostic,
+	DiagnosticSeverity,
+} from 'vscode-languageserver/node';
+
+
+export const statefulValidation = (text: String, diagnostics: Diagnostic[]) => {
+	const endpointPattern = /^\s*(endpoint|func)\s+(\w+)\s*(\w+)(!)?\s*\([^)]*\):\s*$/;
+	const mutatePattern = /^\s*mutate\s*(.*)?$/;
+	const statefulFuncPattern = /^\s*(\w+)!\(([^)]*)\)$/;
+	const lines = text.split(/\r?\n/);
+
+	for (let lineIndex = 0; lineIndex < lines.length; lineIndex++) {
+		const line = lines[lineIndex];
+		const endpointMatch = line.match(endpointPattern);
+
+		if (endpointMatch) {
+			const endpointName = endpointMatch[1];
+			const hasExclamation = !!endpointMatch[4]; // Check if '!' is present
+			let hasMutateKeyword = false;
+			let hasStatefulFunc = false;
+
+			// Check for 'mutate' keyword after the endpoint/func declaration
+			for (let bodyLineIndex = lineIndex + 1; bodyLineIndex < lines.length; bodyLineIndex++) {
+				const bodyLine = lines[bodyLineIndex];
+				if (!(/^\s*$/.test(bodyLine))) { // Skip empty lines
+					if (bodyLine.match(mutatePattern)) {
+						hasMutateKeyword = true;
+						break;
+					}
+					if (bodyLine.match(statefulFuncPattern)) {
+						hasStatefulFunc = true;
+						break;
+					}
+				}
+			}
+
+			if ((hasMutateKeyword || hasStatefulFunc) && !hasExclamation) {
+				const diagnostic: Diagnostic = {
+					severity: DiagnosticSeverity.Error,
+					range: {
+						start: { line: lineIndex, character: 0 },
+						end: { line: lineIndex, character: line.length }
+					},
+					message: `'${endpointName}' is missing the '!' stateful identifier while performing state modifications`,
+					source: 'ex'
+				};
+				diagnostics.push(diagnostic);
+			}
+		}
+	}
+
+	return diagnostics;
+}
---
+++ server/src/server.ts
@@ -0,0 +1,133 @@
+
+import {
+	createConnection,
+	TextDocuments,
+	Diagnostic,
+	ProposedFeatures,
+	InitializeParams,
+	DidChangeConfigurationNotification,
+	CompletionItem,
+	TextDocumentPositionParams,
+	TextDocumentSyncKind,
+	InitializeResult
+} from 'vscode-languageserver/node';
+import { completionItems, completionDetails } from './modules/completion';
+import { TextDocument } from 'vscode-languageserver-textdocument';
+import { statefulValidation } from './modules/validation';
+
+// Create a connection for the server, using Node's IPC as a transport.
+// Also include all preview / proposed LSP features.
+const connection = createConnection(ProposedFeatures.all);
+
+// Create a simple text document manager.
+const documents: TextDocuments<TextDocument> = new TextDocuments(TextDocument);
+
+let hasConfigurationCapability = false;
+let hasWorkspaceFolderCapability = false;
+let hasDiagnosticRelatedInformationCapability = false;
+
+connection.onInitialize((params: InitializeParams) => {
+	const capabilities = params.capabilities;
+
+	// Does the client support the `workspace/configuration` request?
+	// If not, we fall back using global settings.
+	hasConfigurationCapability = !!(
+		capabilities.workspace && !!capabilities.workspace.configuration
+	);
+	hasWorkspaceFolderCapability = !!(
+		capabilities.workspace && !!capabilities.workspace.workspaceFolders
+	);
+	hasDiagnosticRelatedInformationCapability = !!(
+		capabilities.textDocument &&
+		capabilities.textDocument.publishDiagnostics &&
+		capabilities.textDocument.publishDiagnostics.relatedInformation
+	);
+
+	const result: InitializeResult = {
+		capabilities: {
+			textDocumentSync: TextDocumentSyncKind.Incremental,
+			// Tell the client that this server supports code completion.
+			completionProvider: {
+				resolveProvider: true
+			}
+		}
+	};
+	if (hasWorkspaceFolderCapability) {
+		result.capabilities.workspace = {
+			workspaceFolders: {
+				supported: true
+			}
+		};
+	}
+	return result;
+});
+
+connection.onInitialized(() => {
+	if (hasConfigurationCapability) {
+		// Register for all configuration changes.
+		connection.client.register(DidChangeConfigurationNotification.type, undefined);
+	}
+	if (hasWorkspaceFolderCapability) {
+		connection.workspace.onDidChangeWorkspaceFolders(_event => {
+			connection.console.log('Workspace folder change event received.');
+		});
+	}
+});
+
+// Coco configuration settings to be implemented
+interface CocoSettings {
+}
+
+// Cache the settings of all open documents
+const documentSettings: Map<string, Thenable<CocoSettings>> = new Map();
+
+
+
+// Only keep settings for open documents
+documents.onDidClose(e => {
+	documentSettings.delete(e.document.uri);
+});
+
+// The content of a text document has changed. This event is emitted
+// when the text document first opened or when its content has changed.
+documents.onDidChangeContent(change => {
+	validateTextDocument(change.document);
+});
+
+async function validateTextDocument(textDocument: TextDocument): Promise<void> {
+
+	const text = textDocument.getText();
+	const diagnostics: Diagnostic[] = [];
+
+	statefulValidation(text, diagnostics);
+	connection.sendDiagnostics({ uri: textDocument.uri, diagnostics });
+}
+
+connection.onDidChangeWatchedFiles(_change => {
+	// Monitored files have change in VSCode
+	connection.console.log('We received an file change event');
+});
+
+// This handler provides the initial list of the completion items.
+connection.onCompletion(
+	(_textDocumentPosition: TextDocumentPositionParams): CompletionItem[] => {
+		// The pass parameter contains the position of the text document in
+		// which code complete got requested.
+		return completionItems();
+	}
+);
+
+// This handler resolves additional information for the item selected in
+// the completion list.
+connection.onCompletionResolve(
+	(item: CompletionItem): CompletionItem => {
+		return completionDetails(item);
+	}
+);
+
+// Make the text document manager listen on the connection
+// for open, change and close text document events
+documents.listen(connection);
+
+// Listen on the connection
+connection.listen();
